74c025261cc16bbb0e9c07fd7a0fba87ab3627f3
William <william.hp.frisk@gmail.com>
2026-02-11 11:31:37 +0100

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- absolute line numbers
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- gb for go back
+vim.keymap.set("n", "gb", "<C-o>", { noremap = true, silent = true })
